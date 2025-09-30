8c8af96f051cc32ef6bd76b2c60f571b18e7c35f
Abdisa.dev <meetabdisa@gmail.com>
2025-09-30 20:38:03 +0300

    Register user controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,57 @@
-function register(req, res) {
-  res.send("register");
+// db connection
+const dbConnection = require("../db/dbConfig");
+const bcrypt = require("bcrypt");
+const { StatusCodes } = require("http-status-codes");
+
+
+async function register(req, res) {
+  const { username, firstname, lastname, email, password } = req.body;
+
+  if (!email || !password || !firstname || !lastname || !username) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "please provide all required fields" });
+  }
+
+  // Add database logic here
+  try {
+    const [user] = await dbConnection.query(
+      "SELECT user_name, user_id FROM userTable WHERE user_name = ? OR email = ?",
+      [username, email]
+    );
+
+    if (user.length > 0) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "user already registered" });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "password must be at least 8 characters" });
+    }
+
+    // encrypt the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await dbConnection.query(
+      "INSERT INTO userTable (user_name, first_name, last_name, email, password) VALUES (?,?,?,?,?)",
+      [username, firstname, lastname, email, hashedPassword]
+    );
+    return res.status(StatusCodes.CREATED).json({ msg: "user table created" });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "something went wrong, try again later!" });
+  }
 }
 
-function login(req, res) {
+async function login(req, res) {
   res.send("login");
 }
 
-function checkUser(req, res) {
+async function checkUser(req, res) {
   res.send("check user");
 }
 
