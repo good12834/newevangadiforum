1606a04bd0017b5b6f2297e6bc3468d2e005e363
BurukeBMT <burukmaedot16@gmail.com>
2025-09-30 16:38:25 -0400

    update middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,20 +1,29 @@
 const jwt = require("jsonwebtoken");
+require("dotenv").config()
+const { StatusCodes } = require("http-status-codes");
 
+// checks if the token is sent 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "No token provided" });
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      error: "Unauthorized",
+      message: "Authentication invalid",
+    });
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    
+    const { username, id } = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { username, id};
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      error: "Unauthorized",
+      message: "Authentication invalid",
+    });
   }
 };
 
