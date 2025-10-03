94ae52d44ba7ca98e3a81bc393026652450cd8dc
Abdisa.dev <meetabdisa@gmail.com>
2025-10-03 19:23:11 +0300

    Integrated CORS middleware in app.js to enable cross-origin requests. - Added "cors" dependency to package.json and package-lock.json.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 5500;
+const cors = require("cors");
+
+// Enable CORS for all routes
+app.use(cors());
 
 // db connection
 const dbConnection = require("./db/dbConfig");
