cb975ec0047d38c3533055bd4b92e472ba2673cd
Abdisa.dev <meetabdisa@gmail.com>
2025-10-06 16:03:27 +0300

    Update port configuration to use environment variable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5500;
+const port = process.env.PORT || 5500;
 const cors = require("cors");
 
 // Enable CORS for all routes
